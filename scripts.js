cabf7112d6ab560a2157a1efc4e1f48a769ec6e2
MIIGgames <98579374+MIIGgames@users.noreply.github.com>
2023-07-30 12:07:33 -0300

    up

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// O JavaScript aqui é apenas um exemplo simples e pode ser expandido conforme necessário.
+
+// Event listener para o botão de entrar em contato
+const contactButton = document.querySelector('button');
+contactButton.addEventListener('click', function () {
+    alert('Obrigado por entrar em contato. Responderemos em breve!');
+});
